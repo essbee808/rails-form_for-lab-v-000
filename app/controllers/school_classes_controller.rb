dfc40e9698b221464948108aca9e551a4424d6dc
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-04 07:03:27 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,7 +3,7 @@ class SchoolClassesController < ApplicationController
   def show
     @school_class = SchoolClass.find(params[:id])
   end
-  
+
   def new
     @school_class = SchoolClass.new
   end
